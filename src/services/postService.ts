3045f837a74c61c762b8689eca038a411a2a2f02
avraham <2721avi@gmail.com>
2024-10-10 12:23:25 +0300

    add empty func to post service

---
+++ src/services/postService.ts
@@ -0,0 +1,35 @@
+import { IPost, IComment } from "../models/postModel";
+
+const createPostFromService = async (newPost: IPost) => {
+  try {
+  } catch (err) {}
+};
+
+const getAllPosts = async () => {
+  try {
+  } catch (err) {}
+};
+
+const getPostById = async (postId: string) => {
+  try {
+  } catch (err) {}
+};
+
+const updatePostById = async (
+  postId: string,
+  title?: string,
+  content?: string
+) => {
+  try {
+  } catch (err) {}
+};
+
+const addCommentById = async (postId: string, newComment: IComment) => {
+  try {
+  } catch (err) {}
+};
+
+const deletePostById = async (postId: string) => {
+  try {
+  } catch (err) {}
+};
