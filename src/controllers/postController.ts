11d09f58ebb74ce609aebd2cd2650f8dd48b3ffe
avraham <2721avi@gmail.com>
2024-10-10 17:31:38 +0300

    fixed post controller to void

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -12,21 +12,22 @@ import {
 export const createPost = async (
   req: Request,
   res: Response
-): Promise<Response> => {
+): Promise<void> => {
   try {
     const { title, content, author } = req.body;
 
     if (!title || !content || !author) {
-      return res
+      res
         .status(400)
         .json({ message: "Title, content, and author are required" });
+      return;
     }
 
     const newPost = await createPostFromService(req.body);
-    return res.status(201).json({ newPost });
+    res.status(201).json({ newPost });
   } catch (err) {
     console.log(err);
-    return res.status(500).json({ message: "Error creating post", error: err });
+    res.status(500).json({ message: "Error creating post", error: err });
   }
 };
 
@@ -34,59 +35,54 @@ export const createPost = async (
 export const deletePost = async (
   req: Request,
   res: Response
-): Promise<Response> => {
+): Promise<void> => {
   try {
     const postId = req.params.id;
     const { userIdToDeletePost } = req.body;
 
     if (!userIdToDeletePost) {
-      return res.status(400).json({ message: "User ID is required" });
+      res.status(400).json({ message: "User ID is required" });
+      return;
     }
 
     const deletedPost = await deletePostById(postId, userIdToDeletePost);
 
     if (!deletedPost) {
-      return res.status(404).json({ message: "Post not found" });
+      res.status(404).json({ message: "Post not found" });
+      return;
     }
 
-    return res
+    res
       .status(200)
       .json({ message: "Post deleted successfully", post: deletedPost });
   } catch (err) {
-    return res.status(500).json({ message: "Error deleting post", error: err });
+    res.status(500).json({ message: "Error deleting post", error: err });
   }
 };
 
 // Get all posts
-export const getPosts = async (
-  req: Request,
-  res: Response
-): Promise<Response> => {
+export const getPosts = async (req: Request, res: Response): Promise<void> => {
   try {
     const posts = await getAllPosts();
-    return res.status(200).json(posts);
+    res.status(200).json(posts);
   } catch (err) {
-    return res
-      .status(500)
-      .json({ message: "Error fetching posts", error: err });
+    res.status(500).json({ message: "Error fetching posts", error: err });
   }
 };
 
 // Get a single post by ID
-export const getPost = async (
-  req: Request,
-  res: Response
-): Promise<Response> => {
+export const getPost = async (req: Request, res: Response): Promise<void> => {
   try {
     const post = await getPostById(req.params.id);
 
     if (!post) {
-      return res.status(404).json({ message: "Post not found" });
+      res.status(404).json({ message: "Post not found" });
+      return;
     }
 
-    return res.status(200).json(post);
+    res.status(200).json(post);
   } catch (err) {
-    return res.status(500).json({ message: "Error fetching post", error: err });
+    res.status(500).json({ message: "Error fetching post", error: err });
   }
 };
 
@@ -94,28 +90,30 @@ export const getPost = async (
 export const updatePost = async (
   req: Request,
   res: Response
-): Promise<Response> => {
+): Promise<void> => {
   try {
     const id: string = req.params.id;
     const { title, content } = req.body;
 
     if (!title && !content) {
-      return res.status(400).json({
+      res.status(400).json({
         message: "Either title or content must be provided to update",
       });
+      return;
     }
 
     const updatedPost = await updatePostById(id, req.body);
 
     if (!updatedPost) {
-      return res.status(404).json({ message: "Post not found" });
+      res.status(404).json({ message: "Post not found" });
+      return;
     }
 
-    return res
+    res
       .status(200)
       .json({ message: "Post updated successfully", post: updatedPost });
   } catch (err) {
-    return res.status(500).json({ message: "Error updating post", error: err });
+    res.status(500).json({ message: "Error updating post", error: err });
   }
 };
 
@@ -123,30 +121,26 @@ export const updatePost = async (
 export const addComment = async (
   req: Request,
   res: Response
-): Promise<Response> => {
+): Promise<void> => {
   try {
     const postId = req.params.id;
     const newComment = req.body;
 
     if (!newComment.content || !newComment.author) {
-      return res
-        .status(400)
-        .json({ message: "Content and author are required" });
+      res.status(400).json({ message: "Content and author are required" });
+      return;
     }
 
     const comment = await addCommentById(postId, newComment);
 
     if (!comment) {
-      return res.status(404).json({ message: "Post not found" });
+      res.status(404).json({ message: "Post not found" });
+      return;
     }
 
-    return res
-      .status(201)
-      .json({ message: "Comment added successfully", comment });
+    res.status(201).json({ message: "Comment added successfully", comment });
   } catch (err) {
     console.log("Error adding comment:", err);
-    return res
-      .status(500)
-      .json({ message: "Error adding comment", error: err });
+    res.status(500).json({ message: "Error adding comment", error: err });
   }
 };
